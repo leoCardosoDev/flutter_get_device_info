eea9dcf275059aad1f7534efc9f1e341e5bf8f36
Leonardo Cardoso <leocardosodev@gmail.com>
2020-10-23 05:49:28 -0300

    feat: capture device info Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:device_info/device_info.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,108 +11,64 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Device Info'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+  DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
+  List<Widget> textWidgets = [];
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'Informações do dispositivo:',
+              style: TextStyle(fontSize: 20),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            ...textWidgets,
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () async {
+          AndroidDeviceInfo androidInfo = await deviceInfoPlugin.androidInfo;
+          setState(() {
+            textWidgets.add(Text('androidId: ${androidInfo.androidId}'));
+            textWidgets.add(Text('board: ${androidInfo.board}'));
+            textWidgets.add(Text('bootloader: ${androidInfo.bootloader}'));
+            textWidgets.add(Text('brand: ${androidInfo.brand}'));
+            textWidgets.add(Text('device: ${androidInfo.device}'));
+            textWidgets.add(Text('display: ${androidInfo.display}'));
+            textWidgets.add(Text('fingerprint: ${androidInfo.fingerprint}'));
+            textWidgets.add(Text('hardware: ${androidInfo.hardware}'));
+            textWidgets.add(Text('hashCode: ${androidInfo.hashCode}'));
+            textWidgets.add(Text('host: ${androidInfo.host}'));
+            textWidgets.add(Text('id: ${androidInfo.id}'));
+            textWidgets
+                .add(Text('isPhysicalDevice: ${androidInfo.isPhysicalDevice}'));
+            textWidgets.add(Text('manufacturer: ${androidInfo.manufacturer}'));
+            textWidgets.add(Text('model: ${androidInfo.model}'));
+            textWidgets.add(Text('product: ${androidInfo.product}'));
+          });
+        },
         tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: Icon(Icons.refresh),
+      ),
     );
   }
 }
